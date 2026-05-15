774e448cdc83b425566196f95221ce343a1cfd55
hiroaqii <hiroaqii@gmail.com>
2026-05-16 06:06:58 +0900

    feat: add bgg hot xml parser

--- src/bgg/xml.zig
+++ src/bgg/xml.zig
@@ -7,6 +7,7 @@ const Allocator = std.mem.Allocator;
 
 pub const ParseError = error{
     MissingRequiredAttribute,
+    MissingName,
     MissingPrimaryName,
     UnexpectedEndOfDocument,
     UnexpectedElementEnd,
@@ -64,11 +65,44 @@ pub fn parseSearchResponse(allocator: Allocator, bytes: []const u8) ![]model.Gam
     return results.toOwnedSlice(allocator);
 }
 
+pub fn parseHotResponse(allocator: Allocator, bytes: []const u8) ![]model.HotGame {
+    var static_reader: xml_lib.Reader.Static = .init(allocator, bytes, .{});
+    defer static_reader.deinit();
+    const reader = &static_reader.interface;
+
+    var results: std.ArrayList(model.HotGame) = .empty;
+    errdefer {
+        freeHotGameItems(allocator, results.items);
+        results.deinit(allocator);
+    }
+
+    while (true) {
+        const node = try reader.read();
+        switch (node) {
+            .eof => break,
+            .xml_declaration, .comment, .pi, .text, .cdata, .character_reference, .entity_reference => continue,
+            .element_start => {
+                if (std.mem.eql(u8, reader.elementName(), "item")) {
+                    try results.append(allocator, try parseHotItem(allocator, reader));
+                }
+            },
+            .element_end => continue,
+        }
+    }
+
+    return results.toOwnedSlice(allocator);
+}
+
 pub fn freeSearchResults(allocator: Allocator, results: []model.GameSearchResult) void {
     for (results) |item| {
         allocator.free(item.name);
     }
     allocator.free(results);
+}
+
+pub fn freeHotGames(allocator: Allocator, games: []model.HotGame) void {
+    freeHotGameItems(allocator, games);
+    allocator.free(games);
 }
 
 fn parseSearchItem(allocator: Allocator, reader: *xml_lib.Reader) !model.GameSearchResult {
@@ -110,6 +144,63 @@ fn parseSearchItem(allocator: Allocator, reader: *xml_lib.Reader) !model.GameSea
                 return ParseError.UnexpectedElementEnd;
             },
         }
+    }
+}
+
+fn parseHotItem(allocator: Allocator, reader: *xml_lib.Reader) !model.HotGame {
+    const id = try parseU32Attribute(reader, "id");
+    const rank = try parseU32Attribute(reader, "rank");
+
+    var name: ?[]const u8 = null;
+    errdefer if (name) |value| allocator.free(value);
+
+    var thumbnail_url: ?[]const u8 = null;
+    errdefer if (thumbnail_url) |value| allocator.free(value);
+
+    var year_published: ?i32 = null;
+
+    while (true) {
+        const node = try reader.read();
+        switch (node) {
+            .eof => return ParseError.UnexpectedEndOfDocument,
+            .xml_declaration => unreachable,
+            .comment, .pi, .text, .cdata, .character_reference, .entity_reference => continue,
+            .element_start => {
+                if (std.mem.eql(u8, reader.elementName(), "thumbnail")) {
+                    if (thumbnail_url) |old| allocator.free(old);
+                    thumbnail_url = try dupeAttributeValue(allocator, reader, "value");
+                    try reader.skipElement();
+                } else if (std.mem.eql(u8, reader.elementName(), "name")) {
+                    if (name) |old| allocator.free(old);
+                    name = try dupeAttributeValue(allocator, reader, "value");
+                    try reader.skipElement();
+                } else if (std.mem.eql(u8, reader.elementName(), "yearpublished")) {
+                    year_published = try parseI32Attribute(reader, "value");
+                    try reader.skipElement();
+                } else {
+                    try reader.skipElement();
+                }
+            },
+            .element_end => {
+                if (std.mem.eql(u8, reader.elementName(), "item")) {
+                    return .{
+                        .id = id,
+                        .rank = rank,
+                        .name = name orelse return ParseError.MissingName,
+                        .thumbnail_url = thumbnail_url,
+                        .year_published = year_published,
+                    };
+                }
+                return ParseError.UnexpectedElementEnd;
+            },
+        }
+    }
+}
+
+fn freeHotGameItems(allocator: Allocator, games: []model.HotGame) void {
+    for (games) |game| {
+        allocator.free(game.name);
+        if (game.thumbnail_url) |thumbnail_url| allocator.free(thumbnail_url);
     }
 }
 
@@ -178,4 +269,31 @@ test "parse search XML fixture" {
     try std.testing.expectEqual(@as(u32, 278), results[2].id);
     try std.testing.expectEqualStrings("Catan Card Game", results[2].name);
     try std.testing.expectEqual(@as(?i32, 1996), results[2].year_published);
+}
+
+test "parse hot XML fixture" {
+    const bytes = try std.Io.Dir.cwd().readFileAlloc(
+        std.testing.io,
+        Fixture.path(.hot),
+        std.testing.allocator,
+        .limited(1024 * 1024),
+    );
+    defer std.testing.allocator.free(bytes);
+
+    const games = try parseHotResponse(std.testing.allocator, bytes);
+    defer freeHotGames(std.testing.allocator, games);
+
+    try std.testing.expectEqual(@as(usize, 5), games.len);
+
+    try std.testing.expectEqual(@as(u32, 224517), games[0].id);
+    try std.testing.expectEqual(@as(u32, 1), games[0].rank);
+    try std.testing.expectEqualStrings("Brass: Birmingham", games[0].name);
+    try std.testing.expectEqualStrings("https://cf.geekdo-images.com/sZYp_3BTDGjh2unaZfZmuA__thumb/img/example1.jpg", games[0].thumbnail_url.?);
+    try std.testing.expectEqual(@as(?i32, 2018), games[0].year_published);
+
+    try std.testing.expectEqual(@as(u32, 291457), games[4].id);
+    try std.testing.expectEqual(@as(u32, 5), games[4].rank);
+    try std.testing.expectEqualStrings("Gloomhaven: Jaws of the Lion", games[4].name);
+    try std.testing.expectEqualStrings("https://cf.geekdo-images.com/example5__thumb/img/example5.jpg", games[4].thumbnail_url.?);
+    try std.testing.expectEqual(@as(?i32, 2020), games[4].year_published);
 }
